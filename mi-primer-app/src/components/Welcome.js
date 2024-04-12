880f6a26d29cc416f6f27a965889fac351efc22d
JosueDeSantiago <samj828s@gmail.com>
2024-04-11 20:05:18 -0600

    commit useState

--- mi-primer-app/src/components/Welcome.js
+++ mi-primer-app/src/components/Welcome.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 export default function Welcome(props) {
-  console.log(props);
   const { message, name } = props;
+  const [counter, setCounter] = useState(0);
 
   return (
     <div>
       <h1>Hola, {name}</h1>
       <p>{message}</p>
+      <h2>Contador de react con hooks</h2>
+      <h4>El número del contador es {counter} </h4>
+      <button type="submit" onClick={() => setCounter(counter + 1)}>
+        Sumar Contador
+      </button>
     </div>
   );
 }
