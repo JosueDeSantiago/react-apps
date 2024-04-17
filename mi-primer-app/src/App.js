6039b9ccab7e5e22e332e8c9b4b85e6f174587d0
JosueDeSantiago <samj828s@gmail.com>
2024-04-16 19:06:45 -0600

    yarny Router Dom

--- mi-primer-app/src/App.js
+++ mi-primer-app/src/App.js
@@ -1,6 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Welcome from "./components/Welcome";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./pages/Contact";
+import Contact from "./pages/Contact";
+import AboutMe from "./pages/AboutMe";
+import Profile from "./pages/Profile";
+
 
 function App() {
   return (
@@ -8,6 +14,30 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Welcome message="Hola Welcome props" name="Karla" />
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contacto</Link>
+              </li>
+              <li>
+                <Link to="/about">Acerca de mi</Link>
+              </li>
+              <li>
+                <Link to="/profile">Perfil</Link>
+              </li>
+            </ul>
+          </div>
+          <Routes>
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/profile/:name" element={<Profile />} />
+
+          </Routes>
+        </Router>
       </header>
     </div>
   );
--- mi-primer-app/src/components/Welcome.js
+++ mi-primer-app/src/components/Welcome.js
@@ -1,7 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 export default function Welcome(props) {
   const { message, name } = props;
   const [counter, setCounter] = useState(0);
+  const [semaforo, setSemaforo] = useState(false);
+
+  useEffect(() => {
+    console.log(semaforo);
+  }, [semaforo]);
+
+  const contar = () => {
+    // console.log("Entrando en la función de contar");
+    setCounter(counter + 1);
+    setSemaforo(!semaforo);
+  };
+
+  // console.log(semaforo);
 
   return (
     <div>
@@ -9,7 +22,8 @@ export default function Welcome(props) {
       <p>{message}</p>
       <h2>Contador de react con hooks</h2>
       <h4>El número del contador es {counter} </h4>
-      <button type="submit" onClick={() => setCounter(counter + 1)}>
+      <p>El semáforo está en color {semaforo ? "rojo" : "verde"}</p>
+      <button type="submit" onClick={contar}>
         Sumar Contador
       </button>
     </div>
